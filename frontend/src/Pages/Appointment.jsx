dcd7b88f4b74eb1dc4a42439451527c82dd99e36
Kush Patel <kushpatelj@Mac.lan>
2025-06-18 19:07:34 -0700

    changed the apointment page

--- frontend/src/Pages/Appointment.jsx
+++ frontend/src/Pages/Appointment.jsx
@@ -2,7 +2,37 @@ export function Appointment(){
 
     return (
         <div id='apointments'>
-            <h1>Pick a Time to book an Appointment</h1>
+            <h1>Book an Appointment</h1>
+            <label for="time">Pick a Time:</label>
+            <select>
+                <option>8:00 AM</option>
+                <option>8:30 AM</option>
+                <option>9:00 AM</option>
+                <option>9:30 AM</option>
+                <option>10:00 AM</option>
+                <option>10:30 AM</option>
+                <option>11:00 AM</option>
+                <option>11:30 AM</option>
+                <option>12:00 PM</option>
+                <option>12:30 PM</option>
+                <option>1:00 PM</option>
+                <option>1:30 PM</option>
+                <option>2:00 PM</option>
+                <option>2:30 PM</option>
+                <option>3:00 PM</option>
+                <option>3:30 PM</option>
+                <option>4:00 PM</option>
+                <option>4:30 PM</option>
+            </select>
+
+            <label for="date">Pick a Date:</label>
+            <input type="date" id="date" name="date"/>
+
+
+
+
+
+            <button>Schedule Apointment</button>
             
         </div>
     );
