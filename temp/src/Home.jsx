a86efc479e4f772f309e772ae830a14c5ee7647a
Kush Patel <111901629+kushpatelj86@users.noreply.github.com>
2025-06-24 21:06:38 -0700

    Delete temp directory

--- temp/src/Home.jsx
+++
@@ -1,9 +0,0 @@
-export function Home(){
-    return (
-        <div>
-            <h1>Welcome to Health Fix</h1>
-            <p>Where you get your Health Fixed</p>
-        </div>
-        
-    )
-}
