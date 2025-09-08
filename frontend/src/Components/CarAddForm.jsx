23d5f2d5f04cca7dc1859f8e9ef5f0a39a22a1ca
Kush Patel <kushpatelj@Mac.lan>
2025-09-07 21:12:32 -0700

    fixing things

---
+++ frontend/src/Components/CarAddForm.jsx
@@ -0,0 +1,71 @@
+export function CarAddForm({ user_values, handleValsChange, handleCarSubmit }) {
+  return (
+    <form onSubmit={handleCarSubmit} id="car-add-form">
+      <label htmlFor="make">Make:</label>
+      <input
+        type="text"
+        id="make"
+        name="make"
+        value={user_values.make}
+        onChange={handleValsChange}
+        required
+      />
+
+      <label htmlFor="model">Model:</label>
+      <input
+        type="text"
+        id="model"
+        name="model"
+        value={user_values.model}
+        onChange={handleValsChange}
+        required
+      />
+
+      <label htmlFor="year">Year:</label>
+      <input
+        type="number"
+        id="year"
+        name="year"
+        value={user_values.year}
+        onChange={handleValsChange}
+        required
+      />
+
+      <label htmlFor="vin">VIN:</label>
+      <input
+        type="text"
+        id="vin"
+        name="vin"
+        value={user_values.vin}
+        onChange={handleValsChange}
+        required
+      />
+
+      <label htmlFor="mileage">Mileage:</label>
+      <input
+        type="number"
+        id="mileage"
+        name="mileage"
+        value={user_values.mileage}
+        onChange={handleValsChange}
+      />
+
+      <label htmlFor="fuel">Fuel Type:</label>
+      <select
+        id="fuel"
+        name="fuel_type"
+        value={user_values.fuel_type}
+        onChange={handleValsChange}
+        required
+      >
+        <option value="">Select Fuel</option>
+        <option value="Petrol">Petrol</option>
+        <option value="Diesel">Diesel</option>
+        <option value="Electric">Electric</option>
+        <option value="Hybrid">Hybrid</option>
+      </select>
+
+      <button type="submit">Add Car</button>
+    </form>
+  );
+}
