d490f54f8ed5a0a63fe8efbbe10d90a760142a52
Kush Patel <111901629+kushpatelj86@users.noreply.github.com>
2025-09-07 21:04:07 -0700

    Create car.js

---
+++ frontend/src/api/car.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+export  async function  addCar (values) {
+  try {
+    console.log("/User id cretae ",values)
+
+    const res = await axios.post(`http://localhost:8000/car/add_car`, values);
+    return res.data;
+  } catch (err) {
+    console.error("Error updating user:", err);
+    return null;
+  }
+};
